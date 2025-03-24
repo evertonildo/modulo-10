d1d56d64c4fe017954d5e30dcb9bf3f55a8713af
Evertonildo Maia <evertonildo@gmail.com>
2025-03-24 20:33:41 -0300

    add dockerfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  bomdia = `Bom dia, `;
+  hora = new Date();
   title = 'modulo-10';
+  _json = '';
+
+  ngOnInit(): void {
+    this.getData();
+
+    if (this.hora.getHours() < 12) {
+      this.bomdia = `Bom dia, `;
+    } else if (this.hora.getHours() < 18) {
+      this.bomdia = `Bom tarde, `;
+    } else
+      this.bomdia = `Bom noite, `;
+  }
+
+  async getData() {
+    const url = 'http://169.254.169.254/latest/meta-data/public-ipv4';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      this._json = await response.json();
+      console.log(this._json);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
