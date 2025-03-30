22bdb2800f4213713a3a4ffcdccdecc491dd1533
Evertonildo Maia <evertonildo@gmail.com>
2025-03-30 20:14:29 -0300

    install dependencias

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#rm -R /var/www/html/*
-unzip build.zip .
-cp -R ./build/build/browser/* /var/www/html
+aws s3 cp s3://build.modulo-10/build.zip /home/ec2-user/app/build.zip
+unzip /home/ec2-user/app/build.zip
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo service httpd start
-sudo systemctl start apache2
-sudo systemctl enable apache2
+#sudo service httpd start
+#sudo systemctl start apache2
+#sudo systemctl enable apache2
 
+cp -R /home/ec2-user/app/build/build/browser/* /var/www/html
 # sudo systemctl start httpd.service
