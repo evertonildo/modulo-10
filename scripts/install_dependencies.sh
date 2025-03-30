ce2c18e6ff8fbdc3a4ec10e159d66222dbb70358
Evertonildo Maia <evertonildo@gmail.com>
2025-03-30 18:28:03 -0300

    install dependencias

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,5 @@
 sudo yum install -y httpd
 sudo yum install -y unzip
 rm -R /var/www/html/*
-unzip /home/ec2-user/app/build.zip /var/www/html
+unzip build.zip .
+cp -R ./build/browser/* /var/www/html
