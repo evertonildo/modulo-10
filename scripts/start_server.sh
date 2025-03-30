9b0e5e769283bd6fe13a909228e7aa13217df959
Evertonildo Maia <evertonildo@gmail.com>
2025-03-30 20:24:44 -0300

    install dependencias

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#sudo service httpd start
-#sudo systemctl start apache2
-#sudo systemctl enable apache2
-
 cp -R /home/ec2-user/app/build/build/browser/* /var/www/html
+
+sudo service httpd start
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
 # sudo systemctl start httpd.service
